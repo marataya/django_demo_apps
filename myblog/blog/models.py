326c687fafd474153be370b4faef94a12f676fa6
Marat Ayaibergenov <marataya@gmail.com>
2024-06-01 20:12:23 +0500

    amend Post model change slug from CharField to SlugField

--- myblog/blog/models.py
+++ myblog/blog/models.py
@@ -10,7 +10,7 @@ class Post(models.Model):
         PUBLISHED = 'PB', 'Published'
 
     title = models.CharField(max_length=250)
-    slug = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250)
     author = models.ForeignKey(
         settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE,
